e14c5c9ed31668817ba325ea8ccd218765bd4acf
alimgebekov20-eng <alimgebekov20@gmail.com>
2026-07-25 21:06:48 +0300

    Create main.py

---
+++ client/main.py
@@ -0,0 +1,147 @@
+import pygame
+import sys
+import json
+import os
+from game import Game
+from ui import UI, Menu
+from online_manager import OnlineManager
+from save_manager import SaveManager
+
+class FootballGame:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 700))
+        pygame.display.set_caption("⚽ STREET FOOTBALL LEGENDS")
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+        # Менеджеры
+        self.save_manager = SaveManager()
+        self.online_manager = OnlineManager()
+        
+        # Загрузка сохранения
+        self.player_data = self.save_manager.load()
+        self.ui = UI(self.screen, self.player_data)
+        
+        # Состояние
+        self.state = 'menu'  # 'menu', 'game', 'online', 'profile'
+        self.game = None
+    
+    def run(self):
+        """Главный игровой цикл"""
+        while self.running:
+            self.handle_events()
+            
+            if self.state == 'menu':
+                self.ui.draw_menu()
+            elif self.state == 'game':
+                if self.game:
+                    self.game.update()
+                    self.game.draw()
+            elif self.state == 'online':
+                self.ui.draw_online_menu()
+            elif self.state == 'profile':
+                self.ui.draw_profile()
+            
+            pygame.display.flip()
+            self.clock.tick(60)
+        
+        pygame.quit()
+        sys.exit()
+    
+    def handle_events(self):
+        """Обработка событий"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.save_manager.save(self.player_data)
+                self.running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if self.state == 'menu':
+                    self.handle_menu_keys(event.key)
+                elif self.state == 'game':
+                    self.handle_game_keys(event.key)
+                elif self.state == 'online':
+                    self.handle_online_keys(event.key)
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.state == 'menu':
+                    self.handle_menu_click(event.pos)
+                elif self.state == 'online':
+                    self.handle_online_click(event.pos)
+                elif self.state == 'profile':
+                    self.handle_profile_click(event.pos)
+    
+    def handle_menu_keys(self, key):
+        """Обработка клавиш в меню"""
+        if key == pygame.K_1:
+            self.start_offline_game(mode='2x2')
+        elif key == pygame.K_2:
+            self.start_offline_game(mode='3x3')
+        elif key == pygame.K_3:
+            self.start_offline_game(mode='training')
+        elif key == pygame.K_4:
+            self.state = 'online'
+        elif key == pygame.K_5:
+            self.state = 'profile'
+        elif key == pygame.K_6:
+            self.ui.show_leaderboard()
+        elif key == pygame.K_ESCAPE:
+            self.save_manager.save(self.player_data)
+            self.running = False
+    
+    def handle_menu_click(self, pos):
+        """Обработка кликов в меню"""
+        x, y = pos
+        
+        # Кнопки меню
+        buttons = [
+            {'rect': (100, 100, 200, 60), 'action': lambda: self.start_offline_game('2x2')},
+            {'rect': (100, 180, 200, 60), 'action': lambda: self.start_offline_game('3x3')},
+            {'rect': (100, 260, 200, 60), 'action': lambda: self.start_offline_game('training')},
+            {'rect': (100, 340, 200, 60), 'action': lambda: setattr(self, 'state', 'online')},
+            {'rect': (100, 420, 200, 60), 'action': lambda: setattr(self, 'state', 'profile')},
+            {'rect': (100, 500, 200, 60), 'action': self.ui.show_leaderboard},
+        ]
+        
+        for btn in buttons:
+            if btn['rect'][0] < x < btn['rect'][0] + btn['rect'][2] and \
+               btn['rect'][1] < y < btn['rect'][1] + btn['rect'][3]:
+                btn['action']()
+    
+    def handle_game_keys(self, key):
+        """Обработка клавиш в игре"""
+        if self.game:
+            self.game.handle_key_down(key)
+    
+    def start_offline_game(self, mode='2x2'):
+        """Запуск оффлайн игры"""
+        difficulty = self.ui.get_selected_difficulty()
+        character = self.ui.get_selected_character()
+        
+        self.game = Game(self.screen, mode, difficulty, character, self.player_data)
+        self.game.start()
+        self.state = 'game'
+    
+    def start_online_game(self, mode='ranked'):
+        """Запуск онлайн игры"""
+        # Подключаемся к серверу
+        if self.online_manager.connect():
+            # Ищем соперника
+            match_data = self.online_manager.find_match(
+                self.player_data['name'],
+                mode
+            )
+            if match_data:
+                self.game = Game(self.screen, mode, 'online', 
+                               self.player_data, match_data)
+                self.game.start()
+                self.state = 'game'
+            else:
+                self.ui.show_message('Соперник не найден')
+        else:
+            self.ui.show_message('Ошибка подключения к серверу')
+
+if __name__ == '__main__':
+    game = FootballGame()
+    game.run()
